31f4b9b3e368d3a6e669d019a9d09ba8c4bac6f2
victorgusto <victorgusto@gmail.com>
2022-04-12 23:47:46 -0300

    Autenticação, Shared Preferences, BottomNavigator
    Finalizada a autenticação, quando o usuário já está logado, vai direto para a home page.
    Inicio da Home Page. Criado o bottom navigator para que vá para cada tela.

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -1,15 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        title: Text("Home Page"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Victor",
+                          style: TextStyles.titleBoldBackground),
+                    ]),
+                style: TextStyles.titleRegular,
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              )),
+          GestureDetector(
+            onTap: () {
+              print("Clicou no botão adicionar");
+            },
+            child: Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_box,
+                    color: AppColors.background,
+                  )),
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              )),
+        ]),
       ),
     );
   }
--- payflow/lib/shared/models/user_model.dart
+++ payflow/lib/shared/models/user_model.dart
@@ -1,6 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
 class UserModel {
   final String name;
-  final String? photoUrl;
+  final String? photoURL;
 
-  UserModel({required this.name, this.photoUrl});
+  UserModel({required this.name, this.photoURL});
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'],
+      photoURL: map['photoURL'],
+    );
+  }
+
+  factory UserModel.fromJson(String json) =>
+      UserModel.fromMap(jsonDecode(json));
+
+  Map<String, dynamic> toMap() => {
+        "name": name,
+        "photoURL": photoURL,
+      };
+
+  String toJson() => jsonEncode(toMap());
 }
